eb00d0a5d437356490de5e4385449a8e438840e1
Sam Fung <sfung@adobe.com>
2021-04-30 17:02:44 -0700

    Add spa

---
+++ assets/spa.js
@@ -0,0 +1,52 @@
+(function() {
+  const content = {
+    home: {
+      title: "Home",
+      description:
+        "This is home page.",
+    },
+    about: {
+      title: "About",
+      description:
+        "This is about page.",
+    },
+    contact: {
+      title: "Contact",
+      description:
+        "This is contact page.",
+    }
+  };
+
+  function displayContent(viewName) {
+    const div = document.getElementById("spa-content");
+
+    const data = content[viewName];
+
+    div.innerHTML = `${data.title} - ${data.description}`;
+  }
+
+  function changeRoute(viewName) {
+    displayContent(viewName);
+
+    window.location = `#${viewName}`;
+
+    if (typeof adobe !== "undefined" && typeof adobe.target !== "undefined") {
+      adobe.target.triggerView(viewName);
+    }
+  }
+  window.onload = () => {
+    if (window.location.hash) {
+      changeRoute(window.location.hash.replace("#", ""));
+    }
+
+    window.addEventListener(
+      "hashchange",
+      evt => {
+        // eslint-disable-next-line no-unused-vars
+        const [base, route] = evt.newURL.split("#");
+        changeRoute(route);
+      },
+      false
+    );
+  };
+})();
